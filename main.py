afd28ab29568a15a109f28bb506c4f3c1783873f
Francisco Javier Perez Corona <francisco.perez@ibm.com>
2023-08-04 20:42:21 -0600

    refine caase 1

--- main.py
+++ main.py
@@ -4,13 +4,47 @@ from datetime import datetime
 # Create all records on DB
 populate_all()
 
-print('*** Main.py ***')
-test_user = get_user(email='pake.perez@gmail.com')
-print(get_account(user=test_user))
-update_acount(user=test_user, new_values={'balance': 50})
-print(get_account(user=test_user))
+print('***** Main.py *****\n\n')
+#####################################
+#Case 1 One Person dies, and all the money is transferred to the direct relative
+print('***  Case 1 ***')
+deceased_user = get_user(email='erick.martinez@gmail.com')
+deceased_acc = get_account(user=deceased_user)
+benefited_user = get_user(email='mike.castañeda@gmail.com')
+benefited_acc = get_account(user=benefited_user)
+
+## Last Balance on deceased
+deceased_balance = deceased_acc.balance
+
+## Update Benefited balance
+benefited_balance = benefited_acc.balance
+date = datetime.now()
+update_acount(user = benefited_user, new_values={'balance':benefited_balance + deceased_balance})
+
+## Register movements
+create_movement(-deceased_balance, movementType='Transferencia', account=deceased_acc, date=date )
+create_movement(deceased_balance, movementType='Transferencia', account=benefited_acc, date=date )
+
+##Delete deceased account
+delete_account(user=deceased_user)
+
+print(f"Deceased user: {deceased_user}")
+deceased_balance = "${:,.2f}".format(deceased_balance)
+print(f"Deceased last balance: {deceased_balance}")
+print(f"Benefited user: {benefited_user}")
+benefited_balance = "${:,.2f}".format(benefited_balance)
+print(f"Previous benefited balance = ${benefited_balance}")
+new_balance = get_account(user=benefited_user).balance
+new_balance =  "${:,.2f}".format(new_balance)
+print(f"New benefited balance = { new_balance}")
+print('\n')
+
+#####################################
+
 
 # gastamos y fondeamos
+print('***  Case 2 ***')
+test_user = get_user(email='pake.perez@gmail.com')
 date = datetime.now()
 test_account = get_account(user = test_user)
 test_movement = create_movement(amount = 140000, movementType= "Abono", account = test_account, date = date)
